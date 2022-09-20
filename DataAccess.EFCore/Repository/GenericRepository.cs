5e522b75bb30ae3cf7f417599cb4ab1ae3199ab5
dolintim <dolintim@gmail.com>
2022-09-20 10:34:32 -0400

    Removed useless project

--- DataAccess.EFCore/Repository/GenericRepository.cs
+++ DataAccess.EFCore/Repository/GenericRepository.cs
@@ -7,49 +7,47 @@ using System.Text;
 using System.Threading.Tasks;
 
 using DataAccess.EFCore.Interfaces;
-namespace DataAccess.EFCore.Repository
+namespace DataAccess.EFCore.Repository;
+public class GenericRepository<T> : IGenericRepository<T> where T : class
 {
-    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    protected readonly ReportContext _context;
+    public GenericRepository(ReportContext context)
     {
-        protected readonly ReportContext _context;
-        public GenericRepository(ReportContext context)
-        {
-            _context = context;
-        }
+        _context = context;
+    }
 
-        public void Add(T entity)
-        {
-            _context.Set<T>().Add(entity);
-        }
+    public void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
 
-        public void AddRange(IEnumerable<T> entities)
-        {
-            _context.Set<T>().AddRange(entities);
-        }
+    public void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
 
-        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
-        {
-            return _context.Set<T>().Where(expression);
-        }
+    public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
 
-        public IEnumerable<T> GetAll()
-        {
-            return _context.Set<T>().ToList();
-        }
+    public IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
 
-        public T GetById(int id)
-        {
-            return _context.Set<T>().Find(id);
-        }
+    public T GetById(int id)
+    {
+        return _context.Set<T>().Find(id);
+    }
 
-        public void Remove(T entity)
-        {
-            _context.Set<T>().Remove(entity);
-        }
+    public void Remove(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
 
-        public void RemoveRange(IEnumerable<T> entities)
-        {
-            _context.Set<T>().RemoveRange(entities);
-        }
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
     }
 }
