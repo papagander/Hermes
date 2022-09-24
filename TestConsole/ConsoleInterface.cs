1f8bdf7b08386e7e2dbd76c60e6fb839bbfbf62c
dolintim <dolintim@gmail.com>
2022-09-24 16:04:48 -0400

    Creating Test Console Controller to test DataCoreServices and data access layer

---
+++ TestConsole/ConsoleInterface.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.EFCore;
+
+using Services;
+using Services.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    internal class ConsoleDataCoreController
+    {
+        IDataCoreService S { get; set; }
+
+        public ConsoleDataCoreController(ReportContext _context)
+        {
+            S = new DataCoreService(_context);
+        }
+
+        private bool ParseAndRun(string commandString) 
+        {
+            switch (commandString.Split(" ")[0])
+            {
+                case "createconjoiner":
+
+                default:
+                    break;
+            }
+            return false;
+        }
+        private bool CreateConjoiner(string Name) => S.CreateConjoiner(Name) > 0;
+    }
+}
