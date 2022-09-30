7ea6ca5d48541b12f55fed2c615bc981a3acdf1a
dolintim <dolintim@gmail.com>
2022-09-30 15:49:14 -0400

    deleted crappy thing

--- Migration/Interfaces/Models/IReferences.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Domain.Interfaces.Models;
-
-public interface IReferences<TRef> : IIndexed where TRef : IIndexed
-{
-    public int MyTRefId { get; /*set;*/ }
-    public TRef MyTRef { get; /*set;*/ }
-}
